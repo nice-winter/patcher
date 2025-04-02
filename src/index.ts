1b76c5477637e46af53c8abfb5751bf6bb120415
hhui64 <907322015@qq.com>
2025-04-02 11:58:36 +0800

    fix: correct return statement in nuxtUIProPatcher for array handling

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import type { Plugin } from 'vite'
 
 const nuxtUIProPatcher = (nuxtUIProVitePlugin: Plugin | Plugin[]) => {
   if (Array.isArray(nuxtUIProVitePlugin)) {
-    return nuxtUIProVitePlugin.splice(
+    nuxtUIProVitePlugin.splice(
       nuxtUIProVitePlugin.findIndex(
         (item) => item.name === 'nuxt:ui-pro:license'
       ),
       1
     )
+
+    return nuxtUIProVitePlugin
   }
 
   throw new Error('This is not a valid Vite plugin!')
