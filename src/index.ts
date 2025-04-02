901d7277c2aa6b21615934a017d57f561968c4ef
hhui64 <907322015@qq.com>
2025-04-02 22:10:28 +0800

    feat: implement nuxtUIProPatcher for Vite plugin and update package.json exports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
-import type { Plugin } from 'vite'
-
-const nuxtUIProPatcher = (nuxtUIProVitePlugin: Plugin | Plugin[]) => {
-  if (Array.isArray(nuxtUIProVitePlugin)) {
-    nuxtUIProVitePlugin.splice(
-      nuxtUIProVitePlugin.findIndex(
-        (item) => item.name === 'nuxt:ui-pro:license'
-      ),
-      1
-    )
-
-    return nuxtUIProVitePlugin
-  }
-
-  throw new Error('This is not a valid Vite plugin!')
-}
-
-export { nuxtUIProPatcher, nuxtUIProPatcher as uiPro }
+export {
+  nuxtUIProPatcher,
+  nuxtUIProPatcher as uiPro
+} from './patchs/nuxtUIProPatcher'
